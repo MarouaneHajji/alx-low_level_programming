2e2455e1cfe942f1717a9a8adcab544b81e35e40
MarouaneHajji <mrmarouanehajji2002@gmail.com>
2023-06-15 12:56:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with puts function
+ *
+ * Descritpion: A C program taht prints using put function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle\"");
+    return (0);
+}
